df1d6e78f9aa7ce9439087841c64adac4db85131
cath1100 <72418866+cath1100@users.noreply.github.com>
2024-10-16 00:41:07 -0500

    getting gh-pages working

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. TESTINGTESTING new stuff coming soon
+          Edit <code>src/App.js</code> and save to reload. TESTING TESTING new stuff coming soon
         </p>
         <a
           className="App-link"
